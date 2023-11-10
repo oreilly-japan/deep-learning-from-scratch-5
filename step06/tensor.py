922758ad3b1d55a53c03dba3029a50040e41f030
koki0702 <koki0702@gmail.com>
2023-11-10 11:04:00 +0900

    delete all files

--- step06/tensor.py
+++
@@ -1,8 +0,0 @@
-import torch
-
-x = torch.tensor(5.0, requires_grad=True)
-y = 3 * x ** 2
-print(y)
-
-y.backward()
-print(x.grad)
--- step09/tqdm_sample.py
+++
@@ -1,5 +0,0 @@
-from tqdm import tqdm
-import time
-
-for i in tqdm(range(100)):
-    time.sleep(0.1)
