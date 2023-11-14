f1483b29716ffdd37ba4dae26cb94900c6c50f05
MIURA Yasuyuki <kokosabu@gmail.com>
2023-11-15 03:48:15 +0900

    add step03/plot_norm.py

---
+++ step03/plot_norm.py
@@ -0,0 +1,35 @@
+import numpy as np
+import matplotlib.pyplot as plt
+
+def multivariate_normal(x, mu, cov):
+    det = np.linalg.det(cov)
+    inv = np.linalg.inv(cov)
+    D = len(x)
+    z = 1 / np.sqrt((2 * np.pi) ** D * det)
+    y = z * np.exp((x - mu).T @ inv @ (x - mu) / -2.0)
+    return y
+
+mu = np.array([0.5, -0.2])
+cov = np.array([[2.0, 0.3],
+                [0.3, 0.5]])
+
+x = y = np.arange(-5, 5, 0.1)
+X, Y = np.meshgrid(x, y)
+Z = np.zeros_like(X)
+
+for i in range(X.shape[0]):
+    for j in range(X.shape[1]):
+        x = np.array([X[i, j], Y[i, j]])
+        Z[i, j] = multivariate_normal(x, mu, cov)
+
+fig = plt.figure()
+ax1 = fig.add_subplot(1, 2, 1, projection='3d')  
+ax1.set_xlabel('x')
+ax1.set_ylabel('y')
+ax1.plot_surface(X, Y, Z, cmap='jet')
+
+ax2 = fig.add_subplot(1, 2, 2)
+ax2.set_xlabel('x')
+ax2.set_ylabel('y')
+ax2.contour(X, Y, Z)
+plt.show()
