87e1661cdcfb1db6d0c70e5558295522d8a987f1
MIURA Yasuyuki <kokosabu@gmail.com>
2023-11-15 03:47:41 +0900

    update step03/plot_surface.py

--- step03/plot_surface.py
+++ step03/plot_surface.py
@@ -2,6 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+# 5x5 ====================
 X = np.array([[-2, -1, 0, 1, 2],
               [-2, -1, 0, 1, 2],
               [-2, -1, 0, 1, 2],
@@ -16,6 +17,19 @@ Z = X ** 2 + Y ** 2
 
 ax = plt.axes(projection='3d')  # projection='3d'により3d用グラフを指定
 ax.plot_surface(X, Y, Z, cmap='jet')  # cmap='jet'によりグラフの色を指定
+ax.set_xlabel('x')
+ax.set_ylabel('y')
+plt.show()
+
+# np.meshgrid() ====================
+x = np.arange(-2, 2, 0.1)
+y = np.arange(-2, 2, 0.1)
+
+X, Y = np.meshgrid(x, y)
+Z = X ** 2 + Y ** 2
+
+ax = plt.axes(projection='3d')
+ax.plot_surface(X, Y, Z, cmap='jet')
 ax.set_xlabel('x')
 ax.set_ylabel('y')
 plt.show()
