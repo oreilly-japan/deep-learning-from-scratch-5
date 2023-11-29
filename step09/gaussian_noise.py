0e0b04a12901cbe1a2e774fedd10731f4f64fc2f
koki0702 <koki0702@gmail.com>
2023-11-29 11:10:06 +0900

    画像表示を追加

--- step09/gaussian_noise.py
+++ step09/gaussian_noise.py
@@ -25,6 +25,8 @@ preprocess = transforms.ToTensor()
 x = preprocess(image)
 print(x.shape)  # (3, 64, 64)
 
+original_x = x.clone()  # keep original image
+
 def reverse_to_img(x):
     x = x * 255
     x = x.clamp(0, 255)
@@ -36,17 +38,27 @@ T = 1000
 beta_start = 0.0001
 beta_end = 0.02
 betas = torch.linspace(beta_start, beta_end, T)
+imgs = []
 
 for t in range(T):
     if t % 100 == 0:
         img = reverse_to_img(x)
-        #plt.imshow(img)
-        img.save('flower_diffusion_{:04d}.png'.format(t))
-        plt.show()
+        imgs.append(img)
 
     beta = betas[t]
     eps = torch.randn_like(x)
     x = torch.sqrt(1 - beta) * x + torch.sqrt(beta) * eps
+
+# show imgs
+plt.figure(figsize=(15, 6))
+for i, img in enumerate(imgs[:10]):
+    plt.subplot(2, 5, i + 1)
+    plt.imshow(img)
+    plt.title(f'Noise: {i * 100}')
+    plt.axis('off')
+plt.show()
+
+
 
 # ============================================
 # q(x_t|x_{t+1})
@@ -60,5 +72,13 @@ def add_noise(x_0, t, betas):
     x_t = torch.sqrt(alpha_bar) * x_0 + torch.sqrt(1 - alpha_bar) * eps
     return x_t
 
+x = original_x
+
 t = 100
 x_t = add_noise(x, t, betas)
+
+img = reverse_to_img(x_t)
+plt.imshow(img)
+plt.title(f'Noise: {t}')
+plt.axis('off')
+plt.show()
