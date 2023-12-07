98741a34febdcb2e1a90917dfeac6be939c4960d
koki0702 <koki0702@gmail.com>
2023-12-07 11:21:25 +0900

    Update

--- step01/norm_param.py
+++ step01/norm_param.py
@@ -8,23 +8,27 @@ def normal(x, mu=0, sigma=1):
 x = np.linspace(-10, 10, 1000)
 
 # mu ====================
-y0 = normal(x, mu=0)
-y1 = normal(x, mu=-3)
+y0 = normal(x, mu=-3)
+y1 = normal(x, mu=0)
 y2 = normal(x, mu=5)
 
-plt.plot(x, y0, label='$\mu$=0')
-plt.plot(x, y1, label='$\mu$=-3')
+plt.plot(x, y0, label='$\mu$=-3')
+plt.plot(x, y1, label='$\mu$=0')
 plt.plot(x, y2, label='$\mu$=5')
 plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
 
 # sigma ====================
-y0 = normal(x, mu=0, sigma=1)
-y1 = normal(x, mu=0, sigma=0.5)
+y0 = normal(x, mu=0, sigma=0.5)
+y1 = normal(x, mu=0, sigma=1)
 y2 = normal(x, mu=0, sigma=2)
 
-plt.plot(x, y0, label='$\sigma$=1')
-plt.plot(x, y1, label='$\sigma$=0.5')
+plt.plot(x, y0, label='$\sigma$=0.5')
+plt.plot(x, y1, label='$\sigma$=1')
 plt.plot(x, y2, label='$\sigma$=2')
 plt.legend()
+plt.xlabel('x')
+plt.ylabel('y')
 plt.show()
