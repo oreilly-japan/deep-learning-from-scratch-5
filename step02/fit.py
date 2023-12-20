6279c72c42dfca328d874e59701571592e3b1a96
koki0702 <koki0702@gmail.com>
2023-12-20 10:47:08 +0900

    use np.linspace

--- step02/fit.py
+++ step02/fit.py
@@ -12,7 +12,7 @@ sigma = np.std(xs)
 def normal(x, mu=0, sigma=1):
     y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
     return y
-x = np.arange(150, 190)
+x = np.linspace(150, 190, 1000)
 y = normal(x, mu, sigma)
 
 # plot
