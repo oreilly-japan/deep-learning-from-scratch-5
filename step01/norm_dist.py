cae1a3d07f2613da8348207ffc43827d3b77411b
koki0702 <koki0702@gmail.com>
2024-03-06 11:44:16 +0900

    minor change

--- step01/norm_dist.py
+++ step01/norm_dist.py
@@ -3,7 +3,7 @@ import matplotlib.pyplot as plt
 
 
 def normal(x, mu=0, sigma=1):
-    y = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu)**2 / (2 * sigma**2))
+    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
     return y
 
 x = np.linspace(-5, 5, 100)
--- step01/norm_param.py
+++ step01/norm_param.py
@@ -2,7 +2,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 def normal(x, mu=0, sigma=1):
-    y = 1 / (sigma * np.sqrt(2 * np.pi)) * np.exp(-(x - mu)**2 / (2 * sigma**2))
+    y = 1 / (np.sqrt(2 * np.pi) * sigma) * np.exp(-(x - mu)**2 / (2 * sigma**2))
     return y
 
 x = np.linspace(-10, 10, 1000)
